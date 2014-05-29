ac80ea534d0e1e4e65c5450b29b3f49f6cc05784
jj <giovanni.giorgi@nttdata.com>
2014-05-29 20:25:05 +0200

    Spring data oracle does not work

--- src/main/java/com/nttdata/aq/AQDemoBoot.java
+++ src/main/java/com/nttdata/aq/AQDemoBoot.java
@@ -4,8 +4,8 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -16,34 +16,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springsource.loaded.agent.SpringLoadedPreProcessor;
 
-
-
 /**
  * Main entry point
+ * 
  * @author jj
  *
  */
 @Controller
 @EnableAutoConfiguration()
 @ComponentScan
-//@EnableBatchProcessing
+// @EnableBatchProcessing
 public class AQDemoBoot {
+	
+	@RequestMapping("/about")
+	@ResponseBody
+	String about() {
+		return "The AdvancedQueue+Gradle+SpringBoot+SpringLoaded_2014 Demo_is here.";
+	}
 
+	public static void main(String[] args) throws Exception {
+		// ApplicationListener<ApplicationPreparedEvent> sayHello = new
+		// ApplicationListener<ApplicationPreparedEvent>() {
+		//
+		// @Override
+		// public void onApplicationEvent(ApplicationPreparedEvent event) {
+		// System.err.println("\n\n\t DEMO READY\n\n");
+		// }
+		// };
+		SpringApplicationBuilder ab = new SpringApplicationBuilder(
+				AQDemoBoot.class);
+		// ab.application().addListeners(sayHello);
 
-	public static void main(String[] args) throws Exception {   
-//		ApplicationListener<ApplicationPreparedEvent> sayHello = new ApplicationListener<ApplicationPreparedEvent>() {
-//
-//			@Override
-//			public void onApplicationEvent(ApplicationPreparedEvent event) {
-//				System.err.println("\n\n\t DEMO READY\n\n");				
-//			}
-//		};
-		SpringApplicationBuilder ab=new SpringApplicationBuilder(AQDemoBoot.class);
-//		ab.application().addListeners(sayHello);
-		
-		
-		
-    	ab.run(args);
-    
-    }
+		ab.run(args);
+
+	}
 }
